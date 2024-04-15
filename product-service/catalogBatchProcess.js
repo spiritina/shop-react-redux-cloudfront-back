530ceaff16394e64be05e66975d64311c8139f8a
Olga Novak <olga_novak@epam.com>
2024-04-15 21:05:12 +0200

    add SNS

--- product-service/catalogBatchProcess.js
+++ product-service/catalogBatchProcess.js
@@ -3,5 +3,17 @@ const AWS = require('aws-sdk')
 module.exports.catalogBatchProcess = async (event) => {
    event.Records.forEach((product)=> {
         console.log(product)
+        const sns = new AWS.SNS();
+        const snsParams = {
+            Message: "Product Created",
+            TopicArn: process.env.SNS_TOPIC
+        }
+        sns.publish(snsParams, (err, data) => {
+            if(err){
+                console.log("Error: ", error)
+            }else {
+                console.log("Product created:", data.MessageId)
+            }
+        })
    })
 }
