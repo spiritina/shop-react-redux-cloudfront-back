bd991d080f1dae80e89213073640f3650f711932
Olga Novak <olga_novak@epam.com>
2024-05-04 11:08:41 +0200

    update authorization

--- authorization-service/basicAuthorizer.js
+++ authorization-service/basicAuthorizer.js
@@ -19,7 +19,7 @@ module.exports.basicAuthorizer = (event, ctx, cb) => {
         const token = event.authorizationToken;
         const encodedCreds = token.split(" ")[1]
         const buff = Buffer.from(encodedCreds, "base64");
-        const plainCreds = buff.toString('utf-8').split("=");
+        const plainCreds = buff.toString('utf-8').split(":");
         console.log(plainCreds)
         const username = plainCreds[0];
         const password = plainCreds[1];
